ef0bac739edea2d9d1727f0cc9a881ad10abdfcd
Spyros Sakellaropoulos <spyridonas@live.com>
2024-01-09 21:23:45 +0200

    initial request

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -11,4 +11,5 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
---
+++ frontend/src/components/UserList.vue
@@ -0,0 +1,61 @@
+<script>
+
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+  async mounted() {
+    const response = await fetch("http://localhost/user/");
+    this.users = await response.json()
+  }
+}
+</script>
+<!--data() {-->
+
+<!--}-->
+<!--</script>-->
+<template>
+  <h1>User index</h1>
+
+  <table class="table">
+    <thead>
+    <tr>
+      <th>Id</th>
+      <th>username</th>
+      <th>Roles</th>
+      <th>Password</th>
+      <th>User_fathername</th>
+      <th>User_surname</th>
+      <th>User_phoneno_prim</th>
+      <th>User_email_prim</th>
+      <th>User_mobile_prim</th>
+      <th>actions</th>
+    </tr>
+    </thead>
+    <tbody>
+<!--    {% for user in users %}-->
+    <tr v-for="user in users">
+      <td>{{ user.id }}</td>
+      <td>{{ user.user_name }}</td>
+      <td>{{ user.roles  }}</td>
+      <td>{{ user.password }}</td>
+      <td>{{ user.user_fathername }}</td>
+      <td>{{ user.user_surname }}</td>
+      <td>{{ user.user_phoneno_prim }}</td>
+      <td>{{ user.user_email_prim }}</td>
+      <td>{{ user.user_mobile_prim }}</td>
+      <td>
+        <a href="#">show</a>
+        <a href="#">edit</a>
+      </td>
+    </tr>
+<!--    {% else %}-->
+    <tr v-if="users.length === 0">
+      <td colspan="10">no records found</td>
+    </tr>
+<!--    {% endfor %}-->
+    </tbody>
+  </table>
+</template>
